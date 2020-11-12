bee15d8ba7ccbeaf01b1c2f5d2b3dbd34aea43da
Matthew Jabbour <mj1769@students.necsd.net>
2020-11-12 15:17:41 +0000

    chapter 2 test

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
 
-
-public class Chapter2Test {
+public class MyClass {
   public static void main(String[] args) {
-  
+  double myDouble = 0.89;
+ int myint = (int) myDouble,// manual casting:
+ double to int
+int x = scan.nextInt();
+  System.out.println(myDouble);   // Outputs 8.89
+    System.out.println(myInt);      // Outputs 8
+
+    import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+   Scanner scan = new Scanner(System.in);
+   System.out.println("type 1st Number: ");
+
+   int num1 = scan.nextInt();
+
+   System.out.println("Type 2nd Number: ");
+
+   int num2 = scan.nextInt();
+
+   System.out.println(num1 % num2)
+
   }
 }
